6a96fdbcd2c487255bbc50d495bbb44495a6cb90
mloszek <miloszloszek@gmail.com>
2022-06-16 18:28:13 +0200

    htttp options reloading config

---
+++ Controllers/ConfigController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TaskManager.Controllers
+{
+    [Route("config")]
+    [Authorize(Roles = "Admin")]
+    public class ConfigController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpOptions("reload")]
+        public ActionResult Reload()
+        {
+            try
+            {
+                ((IConfigurationRoot)_configuration).Reload();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
